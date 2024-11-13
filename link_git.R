45e3cd53be237cdb0603ec6e6678b236727ec5e0
YuCheng <yu.cheng@natur.cuni.cz>
2024-11-13 15:34:50 +0100

    comment

---
+++ link_git.R
@@ -0,0 +1,24 @@
+library(usethis)
+# replace with your name and email
+usethis::use_git_config(user.name = "YuCheng", user.email = "yu.cheng@natur.cuni.cz")
+
+#link the github and r
+gitcreds::gitcreds_set()
+
+
+# List all git settings
+usethis::git_sitrep()
+
+library(tidyverse)
+
+utils::install.packages("quarto")
+
+utils::install.packages("here")
+utils::install.packages("renv")
+#utils::install.packages("tidyverse")
+
+usethis::create_from_github(
+  repo_spec = "https://github.com/CUNI-SPRouT/SPROuT-Day3-VersionControl.git",
+  destdir = "D:/PHD Charlse Univeristy/SPROuT/GEA/"  )
+
+here::here()
